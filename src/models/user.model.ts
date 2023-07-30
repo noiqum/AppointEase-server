7f18961c7098a58c9561db474831831000060517
onur  coskun <onur.coskun@wunder.is>
2023-07-30 22:33:35 +0300

    chore: user model interface

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,6 +2,14 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import config from "config";
 
+export interface UserDocument extends mongoose.Document {
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
