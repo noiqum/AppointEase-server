95f7a353dd962abfd5c47e0ab9c445a56a77e4ed
onur  coskun <onur.coskun@wunder.is>
2023-07-30 19:08:55 +0300

    feat: mongoDB connetion

--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,4 @@
 export default {
   port: 1337,
+  dbUri: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.apyij9i.mongodb.net/?retryWrites=true&w=majority`,
 };
--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,15 @@
+require("dotenv").config();
 import express from "express";
 import config from "config";
 import log from "./utils/logger";
 import routes from "./routes";
+import connect from "./utils/connect";
+
 const app = express();
 const port = config.get<number>("port");
 
-app.listen(port, () => {
+app.listen(port, async () => {
   log.info("server is running");
+  await connect();
   routes(app);
 });
