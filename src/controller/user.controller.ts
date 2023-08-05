d04e1e1d6edc99f3c74c8b4f9c74f51aa7c2a465
onur  coskun <onur.coskun@wunder.is>
2023-08-05 14:58:38 +0300

    fix: response hide password

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
 import { createUser } from "../service/user.service";
+import { omit } from "lodash";
 import logger from "../utils/logger";
 export async function createUserHandler(req: Request, res: Response) {
   try {
     console.log("user body", req.body);
     const user = await createUser(req.body);
-    return res.status(200).send(user);
+    return res.status(200).send(omit(user.toJSON(), "password"));
   } catch (error: any) {
     logger.error(error);
     return res.status(409).send(error.message);
