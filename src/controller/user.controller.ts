4eb8473d2fe084b99a8356c2f921483c24a87514
noiqum <onurcoskun99@gmail.com>
2023-09-02 22:04:27 +0300

    feat: login route

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { createUser } from "../service/user.service";
 import { omit } from "lodash";
 import logger from "../utils/logger";
+import UserModal from "../models/user.model";
 export async function createUserHandler(req: Request, res: Response) {
   try {
     const user = await createUser(req.body);
@@ -14,7 +15,16 @@ export async function createUserHandler(req: Request, res: Response) {
 
 export async function loginHandler(req: Request, res: Response) {
   try {
-    return res.send("login handler");
+    const user = await UserModal.findOne({ email: req.body.email });
+    if (!user) {
+      return res.sendStatus(401).json({ message: "User not found" });
+    }
+    const isValid = await user.comparePassword(req.body.password);
+    if (!isValid) {
+      return res.sendStatus(401).json({ message: "Incorrect password" });
+    } else {
+      return res.status(200).json(omit(user.toJSON(), "password"));
+    }
   } catch (error: any) {
     logger.error(error);
     return res.status(409).send(error.message);
