7e05f92b6a5bb20388d2936f42d9b9706216fb66
noiqum <onurcoskun99@gmail.com>
2023-09-07 21:43:45 +0300

    fix: cors

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import deserializeUser from "../middleware/deserializeUser";
 import routes from "../routes";
+import cors from "cors";
 
 export function createServer() {
   const app = express();
+  app.use(cors());
   app.use(express.json());
   app.use(deserializeUser);
   app.use(express.urlencoded({ extended: true }));
