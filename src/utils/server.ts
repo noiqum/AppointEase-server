ce061daa2a69d8d98bedc1f3b27c2c26e2b58c3a
onur  coskun <onur.coskun@wunder.is>
2023-11-11 21:43:19 +0300

    feat: cookie parser

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -3,12 +3,13 @@ import deserializeUser from "../middleware/deserializeUser";
 import routes from "../routes";
 import cors from "cors";
 import logger from "../middleware/logger";
-
+import cookieParser from "cookie-parser";
 export function createServer() {
   const app = express();
   app.use(cors());
   app.use(logger);
   app.use(express.json());
+  app.use(cookieParser());
   app.use(deserializeUser);
   app.use(express.urlencoded({ extended: true }));
   routes(app);
