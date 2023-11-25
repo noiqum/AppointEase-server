682206c7fec459e2c9474349dad704432012b949
noiqum <onurcoskun99@gmail.com>
2023-11-25 16:34:07 +0300

    feat: login controller update

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { createUser } from "../service/user.service";
 import { omit } from "lodash";
 import logger from "../utils/logger";
+import  jwt from "jsonwebtoken"
 import UserModal from "../models/user.model";
 export async function createUserHandler(req: Request, res: Response) {
   try {
@@ -23,7 +24,34 @@ export async function loginHandler(req: Request, res: Response) {
     if (!isValid) {
       return res.sendStatus(401).json({ message: "Incorrect password" });
     } else {
-      return res.status(200).json(omit(user.toJSON(), "password"));
+      const accessToken = jwt.sign(
+        {
+            "UserInfo": {
+                "username": user.name,
+                "email":user.email
+            }
+        },
+        process.env.ACCESS_TOKEN_SECRET || "",
+        { expiresIn: '1h' }
+    )
+
+    const refreshToken = jwt.sign(
+        { "username": user.name },
+        process.env.REFRESH_TOKEN_SECRET || "",
+        { expiresIn: '7d' }
+    )
+
+    // Create secure cookie with refresh token 
+    res.cookie('jwt', refreshToken, {
+        httpOnly: true, //accessible only by web server 
+        secure: true, //https
+        sameSite: "none", //cross-site cookie 
+        maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
+    })
+    return res.status(200).json({
+      ...omit(user.toJSON(), "password"),
+      accessToken: accessToken
+    });
     }
   } catch (error: any) {
     logger.error(error);
