be6c9e83bc3193b7ce7fb53c3b39ac5bcb96d760
noiqum <onurcoskun99@gmail.com>
2023-11-26 17:10:58 +0300

    feat: jwt verify middleware

---
+++ src/middleware/jwtVerify.ts
@@ -0,0 +1,39 @@
+import jwt, { VerifyErrors, JwtPayload } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization as string;
+
+  if (!authHeader?.includes('Bearer ')) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  interface CustomRequest extends Request {
+    name: any;
+    email: any;
+  }
+
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET || '',
+    (err: VerifyErrors | null, decoded: JwtPayload | string | undefined) => {
+      if (err) {
+        return res.status(403).json({ message: 'Forbidden' });
+      }
+
+      if (typeof decoded === 'string') {
+        // Handle the case where decoded is a string
+        return res.status(403).json({ message: 'Forbidden' });
+      }
+
+      // Now TypeScript knows that decoded is JwtPayload
+      (req as CustomRequest).name = decoded?.userInfo?.username;
+      (req as CustomRequest).email = decoded?.userInfo?.email;
+      next();
+    }
+  );
+};
+
+module.exports = verifyJWT;
